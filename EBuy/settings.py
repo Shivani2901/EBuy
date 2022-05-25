ec9f91e5b8d58eab6ea91c0126113d77f1f788f9
Shivani <tomar.shivani2479@gmail.com>
2022-05-25 22:41:23 +0530

    five

--- EBuy/settings.py
+++ EBuy/settings.py
@@ -131,9 +131,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tomar.shivani2479@gmail.com'
-EMAIL_HOST_PASSWORD = '2901@Govind'
+EMAIL_HOST_USER = 'ebuy.test22@gmail.com'
+EMAIL_HOST_PASSWORD = 'Test040@ebuy'
 
-
-PAYPAL_RECEIVER_EMAIL =''
-PAYPAL_TEST = True
+#
+# PAYPAL_RECEIVER_EMAIL =''
+# PAYPAL_TEST = True
